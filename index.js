fbaec4333ba44664dbf9bd217dcd38037dcb7f6e
kehinde <fagbamigbekehinde@gmail.com>
2020-12-02 06:34:57 -0600

    new button

--- index.js
+++ index.js
@@ -16,63 +16,72 @@ function initializeViz() {
   viz = new tableau.Viz(placeholderDiv, url, options);
 } 
 
-
-function o() {
-  workbook = workbook.activateSheetAsync("Bain with SSD")
-  workbook.getActiveSheet().applyFilterAsync(filterName,filterValue,filterType);
+function f() {
+  activeSheet.applyFilterAsync(
+    "Category",
+    "Furniture",
+    tableau.FilterUpdateType.ADD);
 } 
 
 
 
-function vizFilter(filterName,filterValue,filterType){
-  sheet=viz.getWorkbook().getActiveSheet();
-  if(sheet.getSheetType() === 'worksheet'){
-  sheet.applyFilterAsync(filterName,filterValue,filterType);
-  }
-  else{
-  worksheetArray = sheet.getWorksheets();
-  for(var i =0; i < worksheetArray.length; i++)
-  {
-  worksheetArray[i].applyFilterAsync(filterName,filterValue,filterType);
-  }
-  }
-  };
 
-
-function category(value) {
-    activeSheet.applyFilterAsync(
-      "Category",
-      value,
-      tableau.FilterUpdateType.ADD);
-  }  
-
-  function f() {
-    activeSheet.applyFilterAsync(
-      "Category",
-      "Furniture",
-      tableau.FilterUpdateType.ADD);
-  } 
-
-  function t() {
-    activeSheet.applyFilterAsync(
-      "Category",
-      "Technology",
-      tableau.FilterUpdateType.ADD);
-  } 
-
-
-  function o() {
-    activeSheet.applyFilterAsync(
-      "Category",
-      "Office Supplies",
-      tableau.FilterUpdateType.ADD);
-  } 
+// function o() {
+//   workbook = workbook.activateSheetAsync("Bain with SSD")
+//   workbook.getActiveSheet().applyFilterAsync(filterName,filterValue,filterType);
+// } 
 
 
 
-  function addValuesToFilter() {
-    activeSheet.applyFilterAsync(
-      "Category",
-      ["Furniture", "Technology", "Office Supplies"],
-      tableau.FilterUpdateType.REMOVE);
-  } 
+// function vizFilter(filterName,filterValue,filterType){
+//   sheet=viz.getWorkbook().getActiveSheet();
+//   if(sheet.getSheetType() === 'worksheet'){
+//   sheet.applyFilterAsync(filterName,filterValue,filterType);
+//   }
+//   else{
+//   worksheetArray = sheet.getWorksheets();
+//   for(var i =0; i < worksheetArray.length; i++)
+//   {
+//   worksheetArray[i].applyFilterAsync(filterName,filterValue,filterType);
+//   }
+//   }
+//   };
+
+
+// function category(value) {
+//     activeSheet.applyFilterAsync(
+//       "Category",
+//       value,
+//       tableau.FilterUpdateType.ADD);
+//   }  
+
+//   function f() {
+//     activeSheet.applyFilterAsync(
+//       "Category",
+//       "Furniture",
+//       tableau.FilterUpdateType.ADD);
+//   } 
+
+//   function t() {
+//     activeSheet.applyFilterAsync(
+//       "Category",
+//       "Technology",
+//       tableau.FilterUpdateType.ADD);
+//   } 
+
+
+//   function o() {
+//     activeSheet.applyFilterAsync(
+//       "Category",
+//       "Office Supplies",
+//       tableau.FilterUpdateType.ADD);
+//   } 
+
+
+
+//   function addValuesToFilter() {
+//     activeSheet.applyFilterAsync(
+//       "Category",
+//       ["Furniture", "Technology", "Office Supplies"],
+//       tableau.FilterUpdateType.REMOVE);
+//   } 
