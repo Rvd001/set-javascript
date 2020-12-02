8a1afa862150c788da42b435a6768690564177fe
kehinde <fagbamigbekehinde@gmail.com>
2020-12-02 05:44:41 -0600

    All files

---
+++ index.js
@@ -0,0 +1,33 @@
+
+function initializeViz() {
+  var placeholderDiv = document.getElementById("tableauViz");
+  var url = "https://public.tableau.com/views/WorldIndicators/GDPpercapita";
+  var options = {
+    width: placeholderDiv.offsetWidth,
+    height: placeholderDiv.offsetHeight,
+    hideTabs: true,
+    hideToolbar: true,
+    onFirstInteractive: function () {
+      workbook = viz.getWorkbook();
+      activeSheet = workbook.getActiveSheet();
+    }
+  };
+  viz = new tableau.Viz(placeholderDiv, url, options);
+} 
+
+
+function category(value) {
+    activeSheet.applyFilterAsync(
+      "Category",
+      value,
+      tableau.FilterUpdateType.ADD);
+  }  
+
+
+
+  function addValuesToFilter() {
+    activeSheet.applyFilterAsync(
+      "Category",
+      ["Furniture", "Technology", "Office Supplies"],
+      tableau.FilterUpdateType.REMOVE);
+  } 
